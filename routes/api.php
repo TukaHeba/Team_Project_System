b43ccb6ae05e63193fa70289df92032365253032
Tuka Heba <tuka.heba@gmail.com>
2024-09-17 19:29:14 +0300

    Task Routes

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::apiResource('users', UserController::class)->middleware(['auth:api', 'adm
 Route::apiResource('projects', ProjectController::class)->middleware(['auth:api', 'admin']);
 
 // Task Routes
-Route::apiResource('tasks', TaskController::class)->middleware(['auth:api', 'admin']);
+// Route::apiResource('tasks', TaskController::class)->middleware(['auth:api', 'admin']);
 
 // Note Routes
 Route::controller(NoteController::class)->group(function () {
@@ -43,4 +43,29 @@ Route::controller(NoteController::class)->group(function () {
     Route::get('tasks/{taskId}/notes/{noteId}', 'show')->middleware('auth:api');
     Route::put('tasks/{taskId}/notes/{noteId}', 'update')->middleware('auth:api');
     Route::delete('tasks/{taskId}/notes/{noteId}', 'destroy')->middleware('auth:api');
+});
+
+// Task Routes
+Route::controller(TaskController::class)->group(function () {
+
+    // Task CRUD Routes with middleware
+    Route::apiResource('tasks', TaskController::class)->middleware('auth:api');
+
+    // Get the latest task for a project
+    Route::get('/projects/{projectId}/latest-task', 'getLatestTaskForProject')->middleware('auth:api');
+
+    // Get the oldest task for a project
+    Route::get('/projects/{projectId}/oldest-task', 'getOldestTaskForProject')->middleware('auth:api');
+
+    // Get the latest task for a user
+    Route::get('/users/{userId}/latest-task', 'getLatestTaskForUser')->middleware('auth:api');
+
+    // Get the oldest task for a user
+    Route::get('/users/{userId}/oldest-task', 'getOldestTaskForUser')->middleware('auth:api');
+
+    // Retrieve all tasks for a given user by projects
+    Route::get('/users/{userId}/tasks-by-projects', 'getTasksByUserProjects')->middleware('auth:api');
+
+    // Update the status of a task
+    Route::patch('/tasks/{task}/status', 'updateTaskStatus')->middleware('auth:api');
 });
